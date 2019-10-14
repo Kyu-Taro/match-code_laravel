847259633f16e14901d2404ecd93942082ab4c62
Kyu Taro <kyutaro@Kyu.local>
2019-10-14 16:48:03 +0900

    commit

--- app/Notifications/VerifyEmailJapanese.php
+++ app/Notifications/VerifyEmailJapanese.php
@@ -9,7 +9,7 @@ use Illuminate\Support\Facades\Config;
 use Illuminate\Notifications\Notification;
 use Illuminate\Notifications\Messages\MailMessage;
 
-class VerifyEmail extends Notification
+class VerifyEmailJapanese extends Notification
 {
     /**
      * The callback that should be used to build the mail message.
